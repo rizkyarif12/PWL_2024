8599267ea38d161c5dc9c041637a8053fc9268ee
rizkyarif12 <116237742+rizkyarif12@users.noreply.github.com>
2024-02-23 09:25:54 +0700

    tambah controller

--- routes/web.php
+++ routes/web.php
@@ -17,42 +17,42 @@ use Illuminate\Support\Facades\Route;
 //     return view('welcome');
 // });
 
-Route::get('/hello', function () {
-    return 'Hello';
-});
+// Route::get('/hello', function () {
+//     return 'Hello';
+// });
 
-Route::get('/world', function () {
-    return 'World';
-});
+// Route::get('/world', function () {
+//     return 'World';
+// });
 
-Route::get('/', function () {
-    return 'Selamat Datang';
-});
+// Route::get('/', function () {
+//     return 'Selamat Datang';
+// });
 
-Route::get('/about', function () {
-    return 'NIM : 2241720040 Nama : Rizky Arifiansyah';
-});
+// Route::get('/about', function () {
+//     return 'NIM : 2241720040 Nama : Rizky Arifiansyah';
+// });
 
 // Route::get('/user/{name}', function ($name) {
 //     return 'Nama saya '.$name;
 // });
 
-Route::get('/posts/{post}/comments/{comment}', function($postId, $commentId) {
-return 'Pos ke-'.$postId. " Komentar ke -: ". $commentId;});
+// Route::get('/posts/{post}/comments/{comment}', function($postId, $commentId) {
+// return 'Pos ke-'.$postId. " Komentar ke -: ". $commentId;});
 
-Route::get('/articles/{id}', function($id) {
-return 'Halaman Artikel dengan ID '.$id;});
+// Route::get('/articles/{id}', function($id) {
+// return 'Halaman Artikel dengan ID '.$id;});
 
 
 // Route::get('/user/{name?}',function ($name=null){
 //     return 'Nama saya '.$name;
 // });
-// Route::get('/user/{name?}',function ($name='John'){
-//     return 'Nama saya '.$name;
-// });
+Route::get('/user/{name?}',function ($name='John'){
+    return 'Nama saya '.$name;
+});
 
-Route::get('/user/profile',function(){
-    //
-})->name('profile');
+// Route::get('/user/profile',function(){
+//     //
+// })->name('profile');
 
 
